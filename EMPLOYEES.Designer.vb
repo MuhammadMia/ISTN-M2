dc5b00f649167837642055ea47389ad7a7d554fd
Mo Mia <mo@syntaxlabs.co.za>
2021-06-21 03:46:15 +0200

    Employee Screen Done

--- EMPLOYEES.Designer.vb
+++ EMPLOYEES.Designer.vb
@@ -24,30 +24,69 @@ Partial Class EMPLOYEES
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
         Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.tblEmployees = New System.Windows.Forms.DataGridView()
+        Me.EMPLOYEESBindingSource = New System.Windows.Forms.BindingSource(Me.components)
         Me.Button8 = New System.Windows.Forms.Button()
         Me.Label1 = New System.Windows.Forms.Label()
-        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
-        Me.EMPLOYEESBindingSource = New System.Windows.Forms.BindingSource(Me.components)
-        Me.EmployeeIDDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.EmployeeNameDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.EmployeeCellDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.EmployeeEmailDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.EmployeePasswordDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.EmployeePositionDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SkyliteDB = New M2.SkyliteDB()
+        Me.SkyliteDBBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.TblEmployeesBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.TblEmployeesTableAdapter = New M2.SkyliteDBTableAdapters.tblEmployeesTableAdapter()
+        Me.DataGridViewTextBoxColumn1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridViewTextBoxColumn2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridViewTextBoxColumn3 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridViewTextBoxColumn4 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridViewTextBoxColumn6 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.txbCell = New System.Windows.Forms.TextBox()
+        Me.txbLName = New System.Windows.Forms.TextBox()
+        Me.txbFName = New System.Windows.Forms.TextBox()
+        Me.txbEmpID = New System.Windows.Forms.TextBox()
+        Me.txbPass = New System.Windows.Forms.TextBox()
+        Me.txbEmail = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txbPosition = New System.Windows.Forms.TextBox()
         Me.Panel1.SuspendLayout()
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.tblEmployees, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.EMPLOYEESBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.SkyliteDB, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.SkyliteDBBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.TblEmployeesBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Panel1
         '
-        Me.Panel1.Controls.Add(Me.DataGridView1)
+        Me.Panel1.Controls.Add(Me.Button2)
+        Me.Panel1.Controls.Add(Me.tblEmployees)
         Me.Panel1.Controls.Add(Me.Button8)
         Me.Panel1.Controls.Add(Me.Label1)
         Me.Panel1.Location = New System.Drawing.Point(12, 12)
         Me.Panel1.Name = "Panel1"
         Me.Panel1.Size = New System.Drawing.Size(776, 426)
         Me.Panel1.TabIndex = 13
+        '
+        'tblEmployees
+        '
+        Me.tblEmployees.AutoGenerateColumns = False
+        Me.tblEmployees.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.tblEmployees.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.DataGridViewTextBoxColumn1, Me.DataGridViewTextBoxColumn2, Me.DataGridViewTextBoxColumn3, Me.DataGridViewTextBoxColumn4, Me.DataGridViewTextBoxColumn6})
+        Me.tblEmployees.DataSource = Me.TblEmployeesBindingSource
+        Me.tblEmployees.Location = New System.Drawing.Point(22, 94)
+        Me.tblEmployees.Name = "tblEmployees"
+        Me.tblEmployees.Size = New System.Drawing.Size(733, 312)
+        Me.tblEmployees.TabIndex = 15
+        '
+        'EMPLOYEESBindingSource
+        '
+        Me.EMPLOYEESBindingSource.DataMember = "EMPLOYEES"
         '
         'Button8
         '
@@ -68,81 +107,236 @@ Partial Class EMPLOYEES
         Me.Label1.TabIndex = 8
         Me.Label1.Text = "EMPLOYEES"
         '
-        'DataGridView1
+        'Button2
         '
-        Me.DataGridView1.AutoGenerateColumns = False
-        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.EmployeeIDDataGridViewTextBoxColumn, Me.EmployeeNameDataGridViewTextBoxColumn, Me.EmployeeCellDataGridViewTextBoxColumn, Me.EmployeeEmailDataGridViewTextBoxColumn, Me.EmployeePasswordDataGridViewTextBoxColumn, Me.EmployeePositionDataGridViewTextBoxColumn})
-        Me.DataGridView1.DataSource = Me.EMPLOYEESBindingSource
-        Me.DataGridView1.Location = New System.Drawing.Point(62, 136)
-        Me.DataGridView1.Name = "DataGridView1"
-        Me.DataGridView1.Size = New System.Drawing.Size(643, 273)
-        Me.DataGridView1.TabIndex = 15
+        Me.Button2.Location = New System.Drawing.Point(694, 3)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(79, 28)
+        Me.Button2.TabIndex = 17
+        Me.Button2.Text = "REMOVE"
+        Me.Button2.UseVisualStyleBackColor = True
         '
+        'SkyliteDB
         '
-        'EMPLOYEESBindingSource
+        Me.SkyliteDB.DataSetName = "SkyliteDB"
+        Me.SkyliteDB.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
         '
-        Me.EMPLOYEESBindingSource.DataMember = "EMPLOYEES"
+        'SkyliteDBBindingSource
         '
-        'EMPLOYEESTableAdapter
+        Me.SkyliteDBBindingSource.DataSource = Me.SkyliteDB
+        Me.SkyliteDBBindingSource.Position = 0
         '
+        'TblEmployeesBindingSource
         '
-        'EmployeeIDDataGridViewTextBoxColumn
+        Me.TblEmployeesBindingSource.DataMember = "tblEmployees"
+        Me.TblEmployeesBindingSource.DataSource = Me.SkyliteDBBindingSource
         '
-        Me.EmployeeIDDataGridViewTextBoxColumn.DataPropertyName = "Employee_ID"
-        Me.EmployeeIDDataGridViewTextBoxColumn.HeaderText = "Employee_ID"
-        Me.EmployeeIDDataGridViewTextBoxColumn.Name = "EmployeeIDDataGridViewTextBoxColumn"
+        'TblEmployeesTableAdapter
         '
-        'EmployeeNameDataGridViewTextBoxColumn
+        Me.TblEmployeesTableAdapter.ClearBeforeFill = True
         '
-        Me.EmployeeNameDataGridViewTextBoxColumn.DataPropertyName = "Employee_Name"
-        Me.EmployeeNameDataGridViewTextBoxColumn.HeaderText = "Employee_Name"
-        Me.EmployeeNameDataGridViewTextBoxColumn.Name = "EmployeeNameDataGridViewTextBoxColumn"
+        'DataGridViewTextBoxColumn1
         '
-        'EmployeeCellDataGridViewTextBoxColumn
+        Me.DataGridViewTextBoxColumn1.DataPropertyName = "Employee_ID"
+        Me.DataGridViewTextBoxColumn1.HeaderText = "Employee_ID"
+        Me.DataGridViewTextBoxColumn1.Name = "DataGridViewTextBoxColumn1"
         '
-        Me.EmployeeCellDataGridViewTextBoxColumn.DataPropertyName = "Employee_Cell"
-        Me.EmployeeCellDataGridViewTextBoxColumn.HeaderText = "Employee_Cell"
-        Me.EmployeeCellDataGridViewTextBoxColumn.Name = "EmployeeCellDataGridViewTextBoxColumn"
+        'DataGridViewTextBoxColumn2
         '
-        'EmployeeEmailDataGridViewTextBoxColumn
+        Me.DataGridViewTextBoxColumn2.DataPropertyName = "Employee_Name"
+        Me.DataGridViewTextBoxColumn2.HeaderText = "Employee_Name"
+        Me.DataGridViewTextBoxColumn2.Name = "DataGridViewTextBoxColumn2"
         '
-        Me.EmployeeEmailDataGridViewTextBoxColumn.DataPropertyName = "Employee_Email"
-        Me.EmployeeEmailDataGridViewTextBoxColumn.HeaderText = "Employee_Email"
-        Me.EmployeeEmailDataGridViewTextBoxColumn.Name = "EmployeeEmailDataGridViewTextBoxColumn"
+        'DataGridViewTextBoxColumn3
         '
-        'EmployeePasswordDataGridViewTextBoxColumn
+        Me.DataGridViewTextBoxColumn3.DataPropertyName = "Employee_Cell"
+        Me.DataGridViewTextBoxColumn3.HeaderText = "Employee_Cell"
+        Me.DataGridViewTextBoxColumn3.Name = "DataGridViewTextBoxColumn3"
         '
-        Me.EmployeePasswordDataGridViewTextBoxColumn.DataPropertyName = "Employee_Password"
-        Me.EmployeePasswordDataGridViewTextBoxColumn.HeaderText = "Employee_Password"
-        Me.EmployeePasswordDataGridViewTextBoxColumn.Name = "EmployeePasswordDataGridViewTextBoxColumn"
+        'DataGridViewTextBoxColumn4
         '
-        'EmployeePositionDataGridViewTextBoxColumn
+        Me.DataGridViewTextBoxColumn4.DataPropertyName = "Employee_Email"
+        Me.DataGridViewTextBoxColumn4.HeaderText = "Employee_Email"
+        Me.DataGridViewTextBoxColumn4.Name = "DataGridViewTextBoxColumn4"
         '
-        Me.EmployeePositionDataGridViewTextBoxColumn.DataPropertyName = "Employee_Position"
-        Me.EmployeePositionDataGridViewTextBoxColumn.HeaderText = "Employee_Position"
-        Me.EmployeePositionDataGridViewTextBoxColumn.Name = "EmployeePositionDataGridViewTextBoxColumn"
+        'DataGridViewTextBoxColumn6
+        '
+        Me.DataGridViewTextBoxColumn6.DataPropertyName = "Employee_Position"
+        Me.DataGridViewTextBoxColumn6.HeaderText = "Employee_Position"
+        Me.DataGridViewTextBoxColumn6.Name = "DataGridViewTextBoxColumn6"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(810, 382)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(265, 56)
+        Me.Button1.TabIndex = 58
+        Me.Button1.Text = "Add Employee"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(807, 284)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(53, 13)
+        Me.Label10.TabIndex = 57
+        Me.Label10.Text = "Password"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(807, 240)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(32, 13)
+        Me.Label9.TabIndex = 56
+        Me.Label9.Text = "Email"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(807, 197)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(54, 13)
+        Me.Label7.TabIndex = 54
+        Me.Label7.Text = "Cellphone"
+        '
+        'txbCell
+        '
+        Me.txbCell.Location = New System.Drawing.Point(897, 194)
+        Me.txbCell.Name = "txbCell"
+        Me.txbCell.Size = New System.Drawing.Size(178, 20)
+        Me.txbCell.TabIndex = 53
+        '
+        'txbLName
+        '
+        Me.txbLName.Location = New System.Drawing.Point(897, 109)
+        Me.txbLName.Name = "txbLName"
+        Me.txbLName.Size = New System.Drawing.Size(178, 20)
+        Me.txbLName.TabIndex = 52
+        '
+        'txbFName
+        '
+        Me.txbFName.Location = New System.Drawing.Point(897, 71)
+        Me.txbFName.Name = "txbFName"
+        Me.txbFName.Size = New System.Drawing.Size(178, 20)
+        Me.txbFName.TabIndex = 51
+        '
+        'txbEmpID
+        '
+        Me.txbEmpID.Location = New System.Drawing.Point(897, 150)
+        Me.txbEmpID.Name = "txbEmpID"
+        Me.txbEmpID.Size = New System.Drawing.Size(178, 20)
+        Me.txbEmpID.TabIndex = 50
+        '
+        'txbPass
+        '
+        Me.txbPass.Location = New System.Drawing.Point(897, 281)
+        Me.txbPass.Name = "txbPass"
+        Me.txbPass.Size = New System.Drawing.Size(178, 20)
+        Me.txbPass.TabIndex = 48
+        Me.txbPass.UseSystemPasswordChar = True
+        '
+        'txbEmail
+        '
+        Me.txbEmail.Location = New System.Drawing.Point(897, 237)
+        Me.txbEmail.Name = "txbEmail"
+        Me.txbEmail.Size = New System.Drawing.Size(178, 20)
+        Me.txbEmail.TabIndex = 47
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Yu Gothic", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(835, 15)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(182, 31)
+        Me.Label5.TabIndex = 46
+        Me.Label5.Text = "Add Employee"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(807, 153)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(67, 13)
+        Me.Label4.TabIndex = 45
+        Me.Label4.Text = "Employee ID"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(807, 74)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(57, 13)
+        Me.Label3.TabIndex = 44
+        Me.Label3.Text = "First Name"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(807, 112)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(58, 13)
+        Me.Label6.TabIndex = 43
+        Me.Label6.Text = "Last Name"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(807, 326)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(44, 13)
+        Me.Label2.TabIndex = 60
+        Me.Label2.Text = "Position"
+        '
+        'txbPosition
+        '
+        Me.txbPosition.Location = New System.Drawing.Point(897, 323)
+        Me.txbPosition.Name = "txbPosition"
+        Me.txbPosition.Size = New System.Drawing.Size(178, 20)
+        Me.txbPosition.TabIndex = 59
         '
         'EMPLOYEES
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(1089, 450)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txbPosition)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.txbCell)
+        Me.Controls.Add(Me.txbLName)
+        Me.Controls.Add(Me.txbFName)
+        Me.Controls.Add(Me.txbEmpID)
+        Me.Controls.Add(Me.txbPass)
+        Me.Controls.Add(Me.txbEmail)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label6)
         Me.Controls.Add(Me.Panel1)
         Me.Name = "EMPLOYEES"
         Me.Text = "EMPLOYEE"
         Me.Panel1.ResumeLayout(False)
         Me.Panel1.PerformLayout()
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.tblEmployees, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.EMPLOYEESBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.SkyliteDB, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.SkyliteDBBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.TblEmployeesBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents Panel1 As Panel
     Friend WithEvents Button8 As Button
     Friend WithEvents Label1 As Label
-    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents tblEmployees As DataGridView
     Friend WithEvents EMPLOYEESBindingSource As BindingSource
     Friend WithEvents EmployeeIDDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
     Friend WithEvents EmployeeNameDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
@@ -150,4 +344,30 @@ Partial Class EMPLOYEES
     Friend WithEvents EmployeeEmailDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
     Friend WithEvents EmployeePasswordDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
     Friend WithEvents EmployeePositionDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents Button2 As Button
+    Friend WithEvents SkyliteDBBindingSource As BindingSource
+    Friend WithEvents SkyliteDB As SkyliteDB
+    Friend WithEvents TblEmployeesBindingSource As BindingSource
+    Friend WithEvents TblEmployeesTableAdapter As SkyliteDBTableAdapters.tblEmployeesTableAdapter
+    Friend WithEvents DataGridViewTextBoxColumn1 As DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn2 As DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn3 As DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn4 As DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn6 As DataGridViewTextBoxColumn
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents txbCell As TextBox
+    Friend WithEvents txbLName As TextBox
+    Friend WithEvents txbFName As TextBox
+    Friend WithEvents txbEmpID As TextBox
+    Friend WithEvents txbPass As TextBox
+    Friend WithEvents txbEmail As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txbPosition As TextBox
 End Class
--- EMPLOYEES.vb
+++ EMPLOYEES.vb
@@ -1,6 +1,52 @@
 ﻿Public Class EMPLOYEES
     Private Sub EMPLOYEES_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'SkyliteDB.tblEmployees' table. You can move, or remove it, as needed.
+        Me.TblEmployeesTableAdapter.Fill(Me.SkyliteDB.tblEmployees)
         'TODO: This line of code loads data into the 'Group26DataSet6.EMPLOYEES' table. You can move, or remove it, as needed.
 
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim selectedEmp As String = tblEmployees.SelectedCells(0).Value
+        Dim empTable As DataTable = SkyliteDB.tblEmployees
+
+        For Each row As DataRow In empTable.Rows
+            If row.ItemArray(0) = selectedEmp Then
+                empTable.Rows.Remove(row)
+                Exit For
+            End If
+        Next
+
+        SkyliteDB.AcceptChanges()
+
+        TblEmployeesTableAdapter.Update(empTable)
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim fname As String = txbFName.Text
+        Dim lname As String = txbLName.Text
+        Dim empID As String = txbEmpID.Text
+        Dim email As String = txbEmail.Text
+        Dim password As String = txbPass.Text
+        Dim position As String = txbPosition.Text
+        Dim cell As String = txbCell.Text
+
+        Dim row As DataRow = SkyliteDB.tblEmployees.NewRow()
+
+        Dim fullname As String = fname + " " + lname
+
+        row("Employee_ID") = empID
+        row("Employee_Name") = fullname
+        row("Employee_Email") = email
+        row("Employee_Telephone") = cell
+        row("Employee_Password") = password
+        row("Employee_Position") = position
+
+        SkyliteDB.tblEmployees.Rows.Add(row)
+        SkyliteDB.AcceptChanges()
     End Sub
 End Class
